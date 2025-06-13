b61d93bb7cc5f5ed3b837b6bc7ab4389a5b3500b
Jonas Platte <jplatte+git@posteo.de>
2025-06-13 13:18:11 +0200

    Switch from thiserror to snafu
    … and rename Error variants.

--- crates/os-info/src/schema.rs
+++ crates/os-info/src/schema.rs
@@ -6,34 +6,35 @@ use crate::{OSInfo, TechnologyCapabilities};
 use std::fs;
 use std::path::Path;
 
-use thiserror::Error;
+use snafu::{ResultExt as _, Snafu};
 
-#[derive(Debug, Error)]
+#[derive(Debug, Snafu)]
 pub enum Error {
-    #[error(transparent)]
-    SerdeError(#[from] serde_json::Error),
-    #[error(transparent)]
-    IoError(#[from] std::io::Error),
+    #[snafu(display("failed to parse JSON"))]
+    JsonParse { source: serde_json::Error },
+
+    #[snafu(display("failed to read file"))]
+    FsRead { source: std::io::Error },
 }
 
 /// Loads and parses an os-info.json string
 pub fn load_os_info(content: &str) -> Result<OSInfo, Error> {
-    Ok(serde_json::from_str(content)?)
+    serde_json::from_str(content).context(JsonParseSnafu)
 }
 
 /// Loads and parses a technology capabilities json string
 pub fn load_technology(content: &str) -> Result<TechnologyCapabilities, Error> {
-    Ok(serde_json::from_str(content)?)
+    serde_json::from_str(content).context(JsonParseSnafu)
 }
 
 /// Loads and parses an os-info.json file from a path
 pub fn load_os_info_from_path<P: AsRef<Path>>(path: P) -> Result<OSInfo, Error> {
-    let content = fs::read_to_string(path)?;
+    let content = fs::read_to_string(path).context(FsReadSnafu)?;
     load_os_info(&content)
 }
 
 /// Loads and parses a technology capabilities json file from a path
 pub fn load_technology_from_path<P: AsRef<Path>>(path: P) -> Result<TechnologyCapabilities, Error> {
-    let content = fs::read_to_string(path)?;
+    let content = fs::read_to_string(path).context(FsReadSnafu)?;
     load_technology(&content)
 }
